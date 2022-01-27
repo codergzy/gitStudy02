c0c1d3d3829f45c8348a38129d6cfc0fbb69d4c8
codergzy <1169789356@qq.com>
2022-01-28 03:11:45 +0800

    this is -m message,first commit

---
+++ src/main/java/com/gzy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.gzy.git;
+
+/**
+ * Author:gzy
+ * Date:2022/1/28
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
