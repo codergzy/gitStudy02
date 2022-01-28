797dee8938863fdbc4733785ef5e1a67adaaa15f
codergzy <1169789356@qq.com>
2022-01-28 15:23:28 +0800

    hot fix2 commit

--- src/main/java/com/gzy/git/GitTest.java
+++ src/main/java/com/gzy/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello git666!!!");
         System.out.println("hello git666!!!");
+        System.out.println("hello git hot fix2");
     }
 }
